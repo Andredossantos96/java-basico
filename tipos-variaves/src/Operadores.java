454b467e997563bb9c752c8b3c4aa0de31d746a6
Andredossantos96 <86696902+Andredossantos96@users.noreply.github.com>
2023-01-05 19:47:37 -0300

    operadores

---
+++ tipos-variaves/src/Operadores.java
@@ -0,0 +1,35 @@
+public class Operadores {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if(condicao1 &&  (7 > 6)){
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2) {
+            System.out.println("Uma duas condições é verdadeira");
+        }
+
+    System.out.println("fim");
+
+
+    }
+}
+
+        // int a, b;
+        // a = 5;
+        // b = 6;
+
+        // String resultado = a == b ? "verdadeiro" : "falso"; // ? operador ternario
+
+        // System.out.println(resultado);
+
+
+    //    int numero = 13;
+    //    numero = - numero;
+    //    System.out.println(numero); 
+        
+    //     System.out.println("----------");
+    //    numero = numero * -1;
+    //    System.out.println(numero);
